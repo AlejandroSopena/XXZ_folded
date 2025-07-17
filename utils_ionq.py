d6ae00e7e45debe82e6b102fa69399b743062d47
Alejandro Sopena <alexsopena15@gmail.com>
2025-07-17 11:13:17 +0400

    fix api

--- utils_ionq.py
+++ utils_ionq.py
@@ -28,7 +28,6 @@ def get_num_gates(circuit):
 def compile_ionq(circuits, optimization_level, nshots, device, compile=True, counts=False, execute=True):
 
     my_api_key = os.getenv("MY_IONQ_API_KEY")
-    my_api_key = "1qY1wVoD82ugCptfaq8MPsAdI4sDcoSW"
     provider = IonQProvider(my_api_key)
 
     if compile:
